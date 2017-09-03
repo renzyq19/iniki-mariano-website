bc1e0755c030d6889350d73fccf974f212ff57ad
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-09-03 15:43:10 +0100

    transferred to layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import bgvideo from '../video/movie.mp4';
+import headshot from '../images-common/headshot.jpg';
 
 import './index.scss';
+
+const NavLink = props => {
+  const pauseOpen = () => {
+    const video = document.querySelector('#bgvideo');
+    const content = document.querySelector('.content');
+    console.log(video);
+    video.pause();
+    video.classList.add('stopFade');
+    content.classList.add('open');
+  };
+  return (
+    <div>
+      <Link to='#' onClick={pauseOpen}>
+        {props.children}
+      </Link>
+    </div>);
+};
+
+
+  
+NavLink.propTypes = {
+  to: PropTypes.string,
+};
+
+const closePlay = () => {
+  const video = document.querySelector('#bgvideo');
+  const content = document.querySelector('.content');
+  video.classList.remove('stopFade');
+  content.classList.remove('open');
+  video.play();
+};
 
 const TemplateWrapper = ({ children }) =>
   <div>
@@ -17,9 +50,32 @@ const TemplateWrapper = ({ children }) =>
         { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
       ]}
     />
-    <div className="wrapper">
-      {children()}
+    <div className='wrapper'>
+      <div className='container' >
+        <h1>
+          <Link to='#' className='name' >
+            Iniki Mariano
+          </Link>
+        </h1>
+        <div className='content' >
+        {children()}
+          <div className='content-close' onClick={closePlay}>
+            <i className="fa fa-close" aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+      <div className='jam'>
+        <div className='nav'>
+          <NavLink to='/portfolio/'>Portfolio</NavLink>
+          <NavLink to='/showreel/'>Showreel</NavLink>
+          <NavLink to='/bio/'>Bio</NavLink>
+          <NavLink to='/contact/'>Contact</NavLink>
+        </div>
+      </div>
     </div>
+    <video poster={headshot} id='bgvideo' playsInline autoPlay muted loop >
+      <source src={bgvideo} type='video/mp4' />
+    </video>
   </div>;
 
 TemplateWrapper.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
-import headshot from '../images-common/headshot.jpg';
-import './index.scss';
-import bgvideo from '../video/movie.mp4';
 
-
-const IndexPage = () => {
-  const closePlay = () => {
-    const video = document.querySelector('#bgvideo');
-    const content = document.querySelector('.content');
-    video.classList.remove('stopFade');
-    content.classList.remove('open');
-    video.play();
-  };
-  return (
-    <div className='container' >
-      <h1>
-        <Link to='#' className='name' >
-          Iniki Mariano
-        </Link>
-      </h1>
-      <div className='content' >
-        <div className='content-close' onClick={closePlay}>
-          <i className="fa fa-close" aria-hidden="true"></i>
-        </div>
-      </div>
-      <div className='jam'>
-        <div className='nav'>
-          <NavLink to='/portfolio/'>Portfolio</NavLink>
-          <NavLink to='/showreel/'>Showreel</NavLink>
-          <NavLink to='/bio/'>Bio</NavLink>
-          <NavLink to='/contact/'>Contact</NavLink>
-        </div>
-      </div>
-      <video poster={headshot} id='bgvideo' playsInline autoPlay muted loop >
-        <source src={bgvideo} type='video/mp4' />
-      </video>
-    </div>);
-};
-
-const NavLink = props => {
-  const pauseOpen = () => {
-    const video = document.querySelector('#bgvideo');
-    const content = document.querySelector('.content');
-    console.log(video);
-    video.pause();
-    video.classList.add('stopFade');
-    content.classList.add('open');
-  };
-  return (
-    <div>
-      <Link to='#' onClick={pauseOpen}>
-        {props.children}
-      </Link>
-    </div>);
-};
-
-
-  
-NavLink.propTypes = {
-  to: PropTypes.string,
-};
+const IndexPage = () => 
+  <div className='dummy'/ >;
 
 export default IndexPage;
